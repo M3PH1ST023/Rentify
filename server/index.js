f338bfd9af93051ec8f0cd840cce75a50ce96fa2
M3PH1ST023 <mephisto23.03.2003@gmail.com>
2024-06-24 16:09:32 +0530

    CORS-update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
     cors({
-        origin: [process.env.CLIENT_URL],
+        origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     })
 );
 
